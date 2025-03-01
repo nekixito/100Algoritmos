b8c9514b83118c50e3323beff7cfa88052c2c34a
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2025-03-01 01:17:24 -0600

    Problema 2

---
+++ Python/Problema002/Problema002.py
@@ -0,0 +1,17 @@
+#Hallar el cociente y el residuo de dos números
+
+#Entrada
+print("Numero 1: ", end="")
+num1 = int(input())
+
+print("Numero 2: ", end="")
+num2 = int(input())
+
+#Proceso
+cociente = num1 // num2
+residuo = num1 % num2
+
+#Salida
+print("")
+print("Cociente:",cociente)
+print("Residuo:",residuo)
