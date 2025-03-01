3fcfbc6fff1765bd616a7405d3976c65f4caa129
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2025-03-01 01:12:17 -0600

    Problema 1

---
+++ Python/Problema001/Problema001.py
@@ -0,0 +1,15 @@
+#Dados dos numeros enteros, hallar la suma
+
+#Entrada
+print("Numero 1: ", end="")
+num1 = int(input())
+
+print("Numero 2: ", end="")
+num2 = int(input())
+
+#Proceso
+suma = num1 + num2
+
+#Salida
+print("")
+print("Suma:",suma)
