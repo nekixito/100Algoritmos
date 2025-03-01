d3573ca5a71e7d2eec3713ffe66eeb9115608245
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2025-03-01 14:19:35 -0600

    Problema 5

---
+++ Python/Problema005/Problema005.py
@@ -0,0 +1,14 @@
+#Hallar la potencia de a^(1/n), donde a y n son numeros enteros positivos
+
+#Entrada
+print("a: ",end="")
+a = int(input())
+
+print("b: ",end="")
+n = int(input())
+
+#Proceso
+radicacion = int(a**(1/n))
+
+#Salida
+print("Radicacion:",radicacion)
