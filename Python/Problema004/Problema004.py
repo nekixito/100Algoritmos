10a1d7357d6d910cf5d6c3f3aec266080a307e61
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2025-03-01 14:16:39 -0600

    Problema 4

---
+++ Python/Problema004/Problema004.py
@@ -0,0 +1,14 @@
+#Hallar la potencia de a^n, donde a y n son numeros enteros positivos
+
+#Entrada
+print("a: ",end="")
+a = int(input())
+
+print("b: ",end="")
+n = int(input())
+
+#Proceso
+potencia = a**n
+
+#Salida
+print("Potencia:",potencia)
